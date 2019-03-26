a784efce6c6bd9487dd3aadc1131bc303c740980
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-03-25 22:27:02 -0700

    Write unit test for pctVotesDiffByState CP

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,35 +2,27 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import ENV from 'pl-election-results/config/environment';
 
-const stateCodes = [
-  'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN',
-  'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH',
-  'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
-  'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
-]
-
 export default Controller.extend({
-  someInput: '',
-  dataToJoin: computed('someInput', function () {
 
-    let joinData = {}
+  electionYears:    null,
+  politicalParties: null,
+  resultsByState:   null,
 
-    stateCodes.forEach(function (stateCode) {
-      joinData[stateCode] = {
-        'votePercent': (Math.random() * 10)
-      }
-    })
+  electionYear: 2008,
+  friendlyParty: '',
+  opposingParty: '',
 
-    return joinData;
+  pctVotesDiffByState: computed('electionYear', 'friendlyParty', 'opposingParty', function(){
+    return {}
   }),
 
-  layersWithData: computed('dataToJoin', function () {
+  layersWithData: computed('pctVotesDiffByState', function () {
     let thisCtrl = this;
     let layers = ENV.APP.mapLayers;
 
     for (let layer of layers) {
       if (layer.id == "states") {
-        layer.dataToJoin = thisCtrl.get('dataToJoin');
+        layer.dataToJoin = thisCtrl.pctVotesDiffByState;
       }
     }
 
--- tests/unit/controllers/application-test.js
+++ tests/unit/controllers/application-test.js
@@ -4,9 +4,178 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | application', function(hooks) {
   setupTest(hooks);
 
+  hooks.beforeEach(async function(){
+    hooks.mockStates = ['AL', 'AK']
+    hooks.mockGivenYearResults = {
+      "2004_AL": {
+        "Bush, George W.": {
+          "id": "2004_AL",
+          "year": 2004,
+          "name": "Bush, George W.",
+          "abbr": "AL",
+          "state": "Alabama",
+          "votes": 1176394,
+          "parties": [
+            "Republican"
+          ]
+        },
+        "Kerry, John F.": {
+          "id": "2004_AL",
+          "year": 2004,
+          "name": "Kerry, John F.",
+          "abbr": "AL",
+          "state": "Alabama",
+          "votes": 693933,
+          "parties": [
+            "Democratic"
+          ]
+        },
+        "Nader, Ralph": {
+          "id": "2004_AL",
+          "year": 2004,
+          "name": "Nader, Ralph",
+          "abbr": "AL",
+          "state": "Alabama",
+          "votes": 6701,
+          "parties": [
+            "Independent"
+          ]
+        },
+        "Badnarik, Michael": {
+          "id": "2004_AL",
+          "year": 2004,
+          "name": "Badnarik, Michael",
+          "abbr": "AL",
+          "state": "Alabama",
+          "votes": 3529,
+          "parties": [
+            "Independent"
+          ]
+        },
+        "Peroutka, Michael": {
+          "id": "2004_AL",
+          "year": 2004,
+          "name": "Peroutka, Michael",
+          "abbr": "AL",
+          "state": "Alabama",
+          "votes": 1994,
+          "parties": [
+            "Independent"
+          ]
+        }
+      },
+      "2004_AK": {
+        "Bush, George W.": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Bush, George W.",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 190889,
+          "parties": [
+            "Republican"
+          ]
+        },
+        "Kerry, John F.": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Kerry, John F.",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 111025,
+          "parties": [
+            "Democratic"
+          ]
+        },
+        "Nader, Ralph": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Nader, Ralph",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 5069,
+          "parties": [
+            "Populist"
+          ]
+        },
+        "Peroutka, Michael A.": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Peroutka, Michael A.",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 2092,
+          "parties": [
+            "Alaskan Independence"
+          ]
+        },
+        "Badnarik, Michael": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Badnarik, Michael",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 1675,
+          "parties": [
+            "Alaska Libertarian"
+          ]
+        },
+        "Cobb, David": {
+          "id": "2004_AK",
+          "year": 2004,
+          "name": "Cobb, David",
+          "abbr": "AK",
+          "state": "Alaska",
+          "votes": 1058,
+          "parties": [
+            "Green"
+          ]
+        }
+      },
+    }
+  })
+
   // Replace this with your real tests.
   test('it exists', function(assert) {
     let controller = this.owner.lookup('controller:application');
     assert.ok(controller);
   });
+
+  test('pctVotesDiffByState computed property has correct calculations', function(assert) {
+    let controller = this.owner.lookup('controller:application');
+
+    controller.set('stateCodes', hooks.mockStates);
+    controller.set('politicalParties', hooks.mockParties);
+    controller.set('resultsByState', hooks.mockParties);
+    controller.set('electionYear', '2004');
+    controller.set('friendlyParty', 'Independent');
+    controller.set('opposingParty', 'Republican');
+
+    let AL_totalVotes         = 1176394 + 693933 + 6701 + 3529 + 1994;
+    let AL_indepVotes         = 6701 + 3529 + 1994;
+    let AL_republicanVotes    = 1176394;
+    let AL_indepVotePct       = (AL_indepVotes      / AL_totalVotes)*100;
+    let AL_republicanVotePct  = (AL_republicanVotes / AL_totalVotes)*100;
+    let AL_votePctDiff        = AL_indepVotePct - AL_republicanVotePct;
+
+    let AK_totalVotes         = 190889 + 111025 + 5069 + 2092 + 1675 + 1058;
+    let AK_indepVotes         = 0;
+    let AK_republicanVotes    = 190889;
+    let AK_indepVotePct       = (AK_indepVotes      / AK_totalVotes)*100;
+    let AK_republicanVotePct  = (AK_republicanVotes / AK_totalVotes)*100;
+    let AK_votePctDiff        = AK_indepVotePct - AK_republicanVotePct;
+
+    let expectedpctVotesDiffByState = {
+      "AL": {
+        "votePctDiff": AL_votePctDiff
+      },
+      "AK": {
+        "votePctDiff": AK_votePctDiff
+      }
+    }
+
+    assert.equal(JSON.stringify(controller.pctVotesDiffByState), JSON.stringify(expectedpctVotesDiffByState), 'pctVotesDiffByState CP gives expected values');
+  });
+
+
 });
